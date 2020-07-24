c82f935d93e6f3f58ffea98e38e4d04da0205ebc
sultan254 <abubakarali3w@gmail.com>
2020-07-24 08:53:49 +0300

    Öptimazation

--- app.js
+++ app.js
@@ -1,85 +1,66 @@
 $(document).ready(()=>{
-  $("button").click((e)=>{
-    e.preventDefault
+  $("button").click(()=>{
+
+  
+
+  
     const username = $("#txt").val()
     const githubAPI = 'https://api.github.com/users/'+username ;
-    console.log(githubAPI)
+
+
+
     function callback(data){ 
+
+
+
+      
+      
+console.log(data)
+      
       
     const repoAPI = 'https://api.github.com/users/'+username + `/repos`
     function getRepo(repo){
+        var htmlCode = '<div class="container-fluid ">'
+        var htmlCode = '<div class="row">'
         $.each(repo,function(index,repo){
-          $(".repo").append(`
-          <div class="container text-center">
-          <div class="row">
-              <div class="col-md-4">
-                  <div class="card bg-transparent" style="width: 18rem;">
-                      <div class="card-body text-center">
-                        <h5 class="card-title"><strong> ${repo.name}</strong></h5>
-                        <h6 class="card-subtitle mb-2 text-muted">
-                              <span class="badge badge-primary  ">Stars : ${repo.stargazers_count}</span>
-                              <span class="badge badge-secondary ">Followers: ${repo.forks_count}</span>
-                              <span class="badge badge-success">Public_gists: ${repo.watchers_count}</span>
+          htmlCode+="<div class=' col-md-4  mt-3 text-center'>"
+          htmlCode += '<div class="card bg-transparent" >'
+          htmlCode += '<div class="card-body text-center">'
+          htmlCode += '<h5 class="card-title mb-2"><strong>'
+          htmlCode+= repo.name
+          htmlCode += "</strong></h5>"
+          htmlCode += "<h6 class='card-subtitle mb-2 text-muted'>"
+          htmlCode += "<span class='badge badge-primary'>"
+          htmlCode += "Stars :" + repo.stargazers_count + "</span>"
+          htmlCode += "<span class='badge badge-secondary'>"
+          htmlCode += "Stars :" + repo.watchers_count + "</span>"
+          htmlCode += "<span class='badge badge-success'>"
+          htmlCode += "Stars :" + repo.forks_count + "</span>" + "<h6>"
+          htmlCode += '<p class="card-text">' + repo.description + "</p>"
+          htmlCode += '<a href= " '+ repo.html_url+' " target="_blank" class="btn btn-primary" >' + "View Repo" + '</a>'
+          htmlCode += "</div>" + "</div>" + "</div>"
 
-                        </h6>
-                        <p class="card-text">${repo.description}</p>
-                        <a href="+${repo.html_url}+" class="btn btn-primary card-link">View Repo</a>
-                      </div>
-                    </div>
-              </div>
-              <div class="col-md-4">
-                  <div class="card bg-transparent" style="width: 18rem;">
-                      <div class="card-body text-center">
-                        <h5 class="card-title">${repo.name}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">
-                            <span class="badge badge-primary  ">Stars : ${repo.stargazers_count}</span>
-                            <span class="badge badge-secondary ">Followers: ${repo.forks_count}</span>
-                            <span class="badge badge-success">Public_gists: ${repo.watchers_count}</span>
-                        </h6>
-                        <p class="card-text">${repo.description}</p>
-                        <a href=" '+${repo.html_url}+ '" class="btn btn-primary card-link">View Repo</a>
-    
-                      </div>
-                    </div>
-              </div>
-              <div class="col-md-4">
-                  <div class="card bg-transparent" style="width: 18rem;">
-                      <div class="card-body text-center">
-                        <h5 class="card-title">${repo.name}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">
-                              <span class="badge badge-primary  ">Stars : ${repo.stargazers_count}</span>
-                              <span class="badge badge-secondary ">Followers: ${repo.forks_count}</span>
-                              <span class="badge badge-success">Public_gists: ${repo.watchers_count}</span>
-                        </h6>
-                        <p class="card-text">${repo.description}</p>
-                        <a href=" '+${repo.html_url}+' " class="btn btn-primary card-link">View Repo</a>
-                        
-                      </div>
-                    </div>
-              </div>
-  
-         
-
-            
-  
-
-          `)
+          
         })
+        htmlCode += "</div>" + "</div>"
+        $(".repo").append(htmlCode)
        
 
     }
 
     $.getJSON(repoAPI,getRepo)
 
+   
 
-     $(".container").html(`
+    
+     $(".profile").html(`
         <div class="container-fluid text-center  mt-5 ">
             <div class="panel panel-default">
             <div class="panel-heading ">
             </div>
             <div class="panel-body">
                 <div class="row">
-                  <div class="col-md-3">
+                  <div class="col-md-3 text-center">
                       <img src="${data.avatar_url}" class="thumbnail img-fluid text-center mb-3" id="avatar"></img>
                       <h3 class="panel-title">${data.name}</h3>
                       <a href="${data.html_url}" class="btn btn-primary btn-block mt-5" target="_blank">View profile</a>
@@ -94,8 +75,14 @@ $(document).ready(()=>{
 
                         <br><br>
 
+                        
+
                           </ul>
-  
+                          <div class="list-group ">
+                                <div class="list-group-item bg-transparent">Company : ${data.company}</div>
+                                <div class="list-group-item bg-secondary">Date Created  :${data.created_at}</div>
+                            </div>
+                  
                   <div>
                 </div>
                 
@@ -108,18 +95,16 @@ $(document).ready(()=>{
 
      `)
       
-      
-    }
-
-   
-
-     
-
-
     
     
-    
-    
+  
+
+}
+
     $.getJSON(githubAPI,callback)
+
+
+
+
   })
 })
